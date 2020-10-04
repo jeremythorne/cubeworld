cf0fd0ddf739b9b2c67349ce7c4f112649da653f
Jeremy Thorne <jez@jezandbecks.co.uk>
2020-10-04 13:46:29 +0100

    a bit more physics - jump one meter

--- Sources/cube/physics.swift
+++ Sources/cube/physics.swift
@@ -1,34 +1,43 @@
 import Foundation
 
-var a:Double = 0
-var v:Double = 0
-var s:Double = 0
-var jumping:Bool = true
+class Physics {
+    var a:Double = 0
+    var v:Double = 0
+    var s:Double = 0
+    var jumping:Bool = false
 
-func step(dt:Double) {
-    a = -9.8
-    if jumping && s < 0.2 {
-        a = 10
-    } else {
-        jumping = false
-        if s < 0.01 {
-            // on ground
-            a = 0
-        }
+    func jump() {
+        jumping = true
     }
-    v += a * dt
-    s += v * dt
-    if s < 0 {
-        s = -s
-        v = -v * 0.5
+
+    func step(dt:Double) {
+        a = -9.8
+        if jumping && s < 0.3 {
+            a = 30
+        } else {
+            jumping = false
+            if s < 0.01 {
+                // on ground
+                a = 0
+            }
+        }
+        v += a * dt
+        s += v * dt
+        if s < 0 {
+            s = -s
+            v = -v * 0.2
+        }
     }
 }
 
+var physics = Physics()
+physics.jump()
+
 for i in 0...100 {
-    step(dt:1.0 / 60)
+    physics.step(dt:1.0 / 60)
     print(i, String(format:"%0.2f", Double(i) / 60), 
-            String(format:"%0.2f", a),
-            String(format:"%0.2f", v),
-            String(format:"%0.2f", s))
+            String(format:"%0.2f", physics.a),
+            String(format:"%0.2f", physics.v),
+            String(format:"%0.2f", physics.s))
 }
 
